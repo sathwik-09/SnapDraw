f20f62f71ebf4ec7125b81a5dffe57b66c5d6265
sathwik <enapothulasathwik09@gmail.com>
2025-04-07 23:20:09 +0530

    initialized ws backend

--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -1,14 +1,70 @@
-import {WebSocketServer} from "ws";
+import {WebSocketServer, WebSocket} from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config({path: '../../.env'});
 
 const wss = new WebSocketServer({port: 8080});
 
+interface User {
+  ws: WebSocket,
+  rooms: string[],
+  userId: string
+}
 
-wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+const users: User[] = [];
 
+
+function checkUser(token: string): string | null {
+  const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+  if(typeof decoded === 'string') {
+    return null;
+  }
+
+  if(!decoded || !decoded.userId) {
+    return null;
+  }
+  return decoded.userId;
+}
+
+wss.on('connection', function connection(ws, request) {
+  const url = request.url;
+  if(!url) {
+    return;
+  }
+  const queryparams = new URLSearchParams(url.split('?')[1]);
+  const token = queryparams.get('token') || "";
+  const userId = checkUser(token);
+
+  if(userId==null) {
+    ws.close();
+    return null;
+  }
+
+  users.push({
+    userId,
+    rooms: [],
+    ws
+  })
+  
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    const parsedData = JSON.parse(data as unknown as string);
+    if(parsedData.type === "join_room") {
+      const user = users.find(x => x.ws === ws);
+      user?.rooms.push(parsedData.roomId);
+    }
+
+    if(parsedData.type === "leave_room") {
+      const user = users.find(x => x.ws === ws);
+      if(!user) {
+        return;
+      } 
+      user.rooms = user?.rooms.filter(x => x === parsedData.room)
+    }
+
+    if(parsedData.type === "chat"){
+      
+    }
   });
 
-  ws.send('pong');
+  
 });
